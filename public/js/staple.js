630133e67cf4e34be2612a4b40c7cbac2495f9ad
ikkai-r <rm.sales92@gmail.com>
2023-07-23 11:12:07 +0800

    modified:   public/js/staple.js 	modified:   views/layouts/main.hbs 	modified:   views/partials/latest-posts-inner.hbs

--- public/js/staple.js
+++ public/js/staple.js
@@ -36,14 +36,60 @@ function convertDateToTxt() {
 
 }
 
-$("#toggle-pass").click(function () {
-    if($("#password").prop('type') == 'text'){
-        $("#password").prop("type", "password");
-        $('#toggle-pass').addClass( "fa-eye-slash" );
-        $('#toggle-pass').removeClass( "fa-eye" );
-    }else if($("#password").prop('type') == 'password'){
-        $("#password").prop("type", "text");
-        $('#toggle-pass').removeClass( "fa-eye-slash" );
-        $('#toggle-pass').addClass( "fa-eye" );
+$("#toggle-pass-log").click(function () {
+    if($("#password-log").prop('type') == 'text'){
+        $("#password-log").prop("type", "password");
+        $('#toggle-pass-log').addClass( "fa-eye-slash" );
+        $('#toggle-pass-log').removeClass( "fa-eye" );
+    }else if($("#password-log").prop('type') == 'password'){
+        $("#password-log").prop("type", "text");
+        $('#toggle-pass-log').removeClass( "fa-eye-slash" );
+        $('#toggle-pass-log').addClass( "fa-eye" );
+    }
+});
+
+
+$("#toggle-pass-reg").click(function () {
+    if($("#password-reg").prop('type') == 'text'){
+        $("#password-reg").prop("type", "password");
+        $('#toggle-pass-reg').addClass( "fa-eye-slash" );
+        $('#toggle-pass-reg').removeClass( "fa-eye" );
+    }else if($("#password-reg").prop('type') == 'password'){
+        $("#password-reg").prop("type", "text");
+        $('#toggle-pass-reg').removeClass( "fa-eye-slash" );
+        $('#toggle-pass-reg').addClass( "fa-eye" );
+    }
+});
+
+$("#toggle-pass-con").click(function () {
+    if($("#confirm-password").prop('type') == 'text'){
+        $("#confirm-password").prop("type", "password");
+        $('#toggle-pass-con').addClass( "fa-eye-slash" );
+        $('#toggle-pass-con').removeClass( "fa-eye" );
+    }else if($("#confirm-password").prop('type') == 'password'){
+        $("#confirm-password").prop("type", "text");
+        $('#toggle-pass-con').removeClass( "fa-eye-slash" );
+        $('#toggle-pass-con').addClass( "fa-eye" );
+    }
+});
+
+$('#password-reg, #confirm-password').on('keyup', function () {
+    if ($('#password-reg').val() == $('#confirm-password').val()) {
+        $('#pass-msg').removeClass('alert-danger');
+        $('#pass-msg').addClass('alert-success');
+        $('#pass-msg').text('Matching password');
+    } else  {
+        $('#pass-msg').addClass('alert-danger');
+        $('#pass-msg').removeClass('alert-success');
+        $('#pass-msg').text('Not matching password');
+    }
+  });
+
+  $("#register-btn").click(function (e) {
+    if ($('#password-reg').val() != $('#confirm-password').val()) {
+        e.preventDefault();
+        $('#pass-msg').addClass('alert-danger');
+        $('#pass-msg').removeClass('alert-success');
+        $('#pass-msg').text('Ensure your passwords are the same before registering.');
     }
 });
