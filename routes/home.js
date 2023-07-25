14931edd27eab8c1909f70dc1a1ada01ac17eb1a
GeckoJnlx3 <103810781+GeckoJnlx3@users.noreply.github.com>
2023-07-25 22:51:31 +0800

    doesnt work pls dont merge

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const Post = require('../server/schema/Post');
 const Tag = require('../server/schema/Tag');
 const Account = require('../server/schema/Account');
 const Comment = require('../server/schema/Comment');
+const { parse } = require("dotenv");
 const router = express.Router();
 
 
@@ -10,12 +11,47 @@ router.get('/', async (req, res) => {
     const maxTextLength = 100;
 
     try {
+        const page = req.query.p || 0;
+        const postsPerPage = 12;
+
         const listofposts = await Post.find().populate({
             path: 'username',
         }).populate({
             path:'tags',
             select: 'tag_name'
-        }).sort({date: 'desc'}).lean();
+        }).sort({date: 'desc'})
+        .skip(page * postsPerPage)
+        .limit(postsPerPage)
+        .lean();
+
+        const listofallposts = Post.find().lean();
+        const postCount = (await listofallposts).length;
+        const parsedPage = parseInt(page);
+
+        var maxPage = Math.floor(postCount/postsPerPage)
+        var firstPage = 0;
+        var lastPage = maxPage;
+        var prevPage, nextPage;
+
+        prevPage = parsedPage - 1;
+        nextPage = parsedPage + 1;
+
+        if (prevPage < firstPage){
+          prevPage = undefined;
+        } 
+        
+        if (nextPage > maxPage) {
+          nextPage = undefined;
+        } 
+
+        console.log("--debug--");
+        console.log(firstPage);
+        console.log(prevPage);
+        console.log(parsedPage);
+        console.log(nextPage);
+        console.log(lastPage);
+
+
 
         listofposts.forEach((post) => {
 
@@ -49,7 +85,7 @@ router.get('/', async (req, res) => {
 
         for (var i = 0; i < tagCounts.length; i++){
             var newTag = await Tag.findById(tagCounts[i]._id).lean();
-            console.log(tagCounts[i].count);
+            // console.log(tagCounts[i].count);
             var tag = ({
                 tag_name: newTag.tag_name,
                 count: tagCounts[i].count
@@ -59,12 +95,17 @@ router.get('/', async (req, res) => {
         }
 
         res.render("index", {
-        header: "Hot Posts",
-        script: 'js/index.js',
-        posts: listofposts,
-        posts_latest: latest_posts,
-        popular_tags: getPopularTags,
-        navbar: 'navbar'
+          header: "Hot Posts",
+          script: 'js/index.js',
+          posts: listofposts,
+          posts_latest: latest_posts,
+          popular_tags: getPopularTags,
+          navbar: 'navbar',
+          first_page: firstPage,
+          prev_page: prevPage,
+          curr_page: parsedPage,
+          next_page: nextPage,
+          last_page: lastPage
         });
     } catch(error){
         console.log(error);
